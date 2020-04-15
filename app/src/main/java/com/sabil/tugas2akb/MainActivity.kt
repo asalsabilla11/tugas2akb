2e825f8f093f8a4955ccfcc1b4f6f0cb673f4b92
annisa salsabilla <annisasalsa27@gmail.com>
2020-04-16 03:31:26 +0700

    sudah membuat tampilan 1-4

--- app/src/main/java/com/sabil/tugas2akb/MainActivity.kt
+++ app/src/main/java/com/sabil/tugas2akb/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.sabil.tugas2akb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//Nama : Annisa Salsabilla
+//Nim  : 10117196
+//Kelas: IF5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TvRegister.setOnClickListener(){
+            val intent = Intent(this,registeractivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
